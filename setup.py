b1c51360f87c5eb3fd52c2ac1e9321248cd9fb90
lakumbhavdip <lakumbhavdip555@gmail.com>
2025-12-24 12:04:59 +0530

    chore: Modified package name and version in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="fastapi-starter",
-    version="0.1.0",
+    name="fastapi-pro-temp-generator",
+    version="0.1.2",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
